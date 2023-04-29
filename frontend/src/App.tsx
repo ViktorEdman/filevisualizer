540ff0d4a3be42060bc7dd3adf1d0ccbbb212870
Viktor Edman <viktor@edman.se>
2023-04-29 16:01:38 +0200

    Working file tree parsing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,37 @@
-type directory = {
-    name: string;
-    parent?: string;
-    children: directory[];
-};
+type treeNode = {
+    name: string,
+    children?: treeNode[]
+}
 
-function getFolderTree(fileList: string[]): directory {
-    const root: directory = {
-        name: "/",
-        children: [],
-    };
+function getFileTree(fileList: string[]) {
+    const fileTree: treeNode[] = []
 
-    for (let i = 0; i < fileList.length; i++) {
-      const pathLevels = fileList[i].split('/')
-      const child = {
-        name: pathLevels[0],
-        children: []
-      }
-      for (let i = 0; i < pathLevels.length; i++) {
-        const element = pathLevels[i];
-        
-      }
-      root.children.push(child)
-    }
-
-    return root;
+    //Loop over list of files where each file is the path of a single file
+    // for example "marvel/black_widow/bw.png"
+    fileList.forEach((file) => {
+        //Split each file´s path into individual  parts
+        // for example [ "marvel", "black_widow", "bw.png"]
+        const parts = file.split('/');
+        let currentLevel = fileTree
+        //Loop over each level in the path of the file
+        parts.forEach((part) => {
+            // See if current level already has a node with the same name as the part
+            const matchingPart = currentLevel.findIndex((node) => node.name === part)
+            // if not, add a node with the name of the part
+            if (matchingPart === -1) {
+                // save new node to be able to access it by reference
+                let newNode = { name: part, children: [] };
+                currentLevel.push(newNode);
+                // set the new level to be the children array of the new node and
+                // and run the next iteration in that context
+                currentLevel = newNode.children
+            } else {
+                // if there is a matching name at this level already, go to the next iteration at that node's children
+                currentLevel = currentLevel[matchingPart].children || [];
+            }
+        });
+      });
+      return fileTree;
 }
 
 function App() {
@@ -37,15 +45,18 @@ function App() {
         "fact_marvel_beats_dc.txt",
         "marvel/black_widow/why-the-widow-is-awesome.txt",
     ];
-    const fileTree = getFolderTree(list);
+    const fileTree = getFileTree(list);
+    console.log(fileTree)
 
     return (
         <>
             <h1>Hello world</h1>
             <h2>Raw list</h2>
             <pre>{JSON.stringify(list, null, 2)}</pre>
-            <h2>Parsed tree</h2>
-            <pre>{JSON.stringify(fileTree, null, 2)}</pre>
+            <h2>VSCode viz</h2>
+            <ul>
+
+            </ul>
         </>
     );
 }
